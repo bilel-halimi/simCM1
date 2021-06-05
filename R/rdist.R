06401487bb3b449cddb33fb2808d1b37b835cc16
bilel-halimi <84568096+bilel-halimi@users.noreply.github.com>
2021-06-05 17:09:09 +0200

    Update rdist.R

--- R/rdist.R
+++ R/rdist.R
@@ -2,6 +2,7 @@
 #'@export
 #'@param x numeric vector
 #'@param p numeric vector
+#'@param n entier representing numbre of iteration
 rdist<-function(x,p)
 {
   n=length(p)
@@ -32,4 +33,31 @@ rdist<-function(x,p)
   y=x[n]
   return(y)
   }
+}
+rdistn <- function(x,p,n)
+{
+  i=1
+  y=c(i : n-1,1)
+  for (i in 1:n){
+    y[i]=rdist(x,p)
+    i=i+1
+  }
+  return(y)
+}
+Affn<- function (x,p,n){
+  m =rdistn(x,p,n)
+  plot(m,col="green", main = "le nuage de point ")
+
+}
+AffBox<-function(x,p,n){
+  m=rdistn(x,n,p)
+  boxplot(m)
+}
+Moy<- function (x,p,n){
+  m= mean(rdistn(x,p,n))
+  return(m)
+}
+Vari <- function(x,p,n){
+  m= var(rdistn(x,p,n))
+  return(m)
 }
